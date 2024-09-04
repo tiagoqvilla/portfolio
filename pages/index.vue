0bc6ae6b9936f6445b3f6465bab810a3fa53cdec
Tiago Villa <tiago.villa@edu.pucrs.br>
2024-09-04 20:17:44 -0300

    add Posts components

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+const { data: posts } = await useAsyncData('latest-posts', () => queryContent('/blog').sort({ date: 1 }).limit(3).find())
+
+</script>
 
 <template>
     <section>
@@ -27,5 +30,8 @@
 
     <section>
         <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+        <section class="grid md:grid-cols-3 mt-8 gap-10">
+            <Post :posts="posts" />
+        </section>
     </section>
 </template>

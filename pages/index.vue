0d2d786296c7ab80dcf16aaa2969f073c7e8be2a
Tiago Villa <tiago.villa@edu.pucrs.br>
2025-02-26 15:37:33 +0100

    fix typos

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,11 @@ const { data: posts } = await useAsyncData('latest-posts', () => queryContent('/
     <section class="flex flex-col md:flex-row">
         <div class="md:w-3/4">
             <h2 class="text-3xl font-bold mt-8">I'm Tiago Villa.</h2>
-            <p class="text-lg py-2">I am a brazilian software developer currently based in the Netherlands. My
-                current tech stack includes stuff like PHP, TypeScript, React and Vue, but it is always changing and
+            <p class="text-lg py-2">I am a brazilian software developer based in the Netherlands. My tech stack includes
+                stuff like PHP, TypeScript, React and Vue, but it is always changing and
                 expanding.</p>
             <p class="text-lg py-2">In my spare time, I'm currently learning and dabbling with creative development and
-                3D modelling, focusing in technologies like Three.js and WebGL to create immersive and interactive
+                3D modeling, focusing in technologies like Three.js and WebGL to create immersive and interactive
                 experiences on the web.</p>
             <p class="text-lg py-2">You can find more about me on <a class="underline decoration-sky-500"
                     :href="LINKEDIN_LINK">LinkedIn</a> and <a class="underline decoration-sky-500"
